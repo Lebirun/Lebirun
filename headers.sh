9c38595e4e4c544cccfca18919d9779972dbfdb3
opuadm <opuadm@gmail.com>
2025-12-18 22:39:38 +0100

    small fixes

--- headers.sh
+++ headers.sh
@@ -10,11 +10,3 @@ for PROJECT in $SYSTEM_HEADER_PROJECTS; do
     (cd "$PROJECT" && DESTDIR="$SYSROOT" $MAKE install-headers)
   fi
 done
-
-mkdir -p include
-for PROJECT in $SYSTEM_HEADER_PROJECTS; do
-  if [ -d "$PROJECT/include" ]; then
-    echo "Copying $PROJECT headers to top-level include/"
-    cp -R --preserve=timestamps "$PROJECT/include/." include/ || true
-  fi
-done
