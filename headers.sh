f04b7823f1a131d7e065a6c27a175e0a34b79810
opuadm <opuadm@gmail.com>
2026-07-11 23:29:12 +0200

    fix 6

--- headers.sh
+++ headers.sh
@@ -8,7 +8,7 @@ for PROJECT in $SYSTEM_HEADER_PROJECTS; do
   if [ -d "$PROJECT" ]; then
     printf "\033[1;36mInstalling headers for %s...\033[0m\n" "$PROJECT"
     if [ "$PROJECT" = "libc/leblibc" ]; then
-      (cd "$PROJECT" && DESTDIR="$SYSROOT" ARCH=x86_64 prefix=/usr includedir=/usr/include libdir=/usr/lib $MAKE install-headers)
+      (cd "$PROJECT" && $MAKE DESTDIR="$SYSROOT" ARCH=x86_64 prefix=/usr includedir=/usr/include libdir=/usr/lib install-headers)
     else
       (cd "$PROJECT" && DESTDIR="$SYSROOT" $MAKE install-headers)
     fi
